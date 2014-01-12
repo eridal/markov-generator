8ae41d87614207741b3a3c3629d012f40d829d8f
Martin Scotta <martinscotta@gmail.com>
2014-01-12 14:52:41 -0300

    run script

---
+++ markov.php
@@ -0,0 +1,29 @@
+<?php
+
+require 'vendor/autoload.php';
+
+if (count($argv) < 2) {
+    exit("USAGE: {$argv[0]} file-1 file-2 ...");
+} else {
+    array_shift($argv);
+}
+
+$markov = new MarkovGenerator();
+
+$min = null;
+$max = null;
+
+while (count($argv) > 0) {
+    $lines = explode(PHP_EOL, file_get_contents(array_shift($argv)));
+
+    foreach ($lines as $sentence) {
+        if ($sentence) {
+            $min = min($min, strlen($sentence));
+            $max = max($max, strlen($sentence));
+            $markov->learn($sentence);
+        }
+    }
+}
+
+$length = rand($min, $max);
+echo $markov->generate($length), PHP_EOL;
